9257e5290be16d1cab658d30e8470849baeabb5b
Owen O'Malley <theowen@email.com>
2026-02-19 00:17:51 -0800

    Initial Step -> USD export

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,3 +1,15 @@
-./build/bin/GeomTest --inputSTEPFile ./test/step/model1.STEP
-#./build/bin/GeomTest --inputSTEPFile ./test/step/model2.STEP
-#./build/bin/GeomTest --inputSTEPFile ../../Mk10/MK10.STEP
+./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model1.STEP --outputDir ./build/
+#./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model2.STEP
+#!/bin/bash
+
+start=$(date +%s)
+
+#./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputDir ./build/
+
+end=$(date +%s)
+elapsed=$((end - start))
+
+echo "Time taken: ${elapsed} seconds"
+
+# For MK10 the cached usd write takes 68 seconds
+# this is a 700MB heavy Step file 
