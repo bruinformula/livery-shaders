841987275f805afa04c7b98639e9770ff11c2140
Owen O'Malley <theowen@email.com>
2026-02-20 00:36:43 -0800

    Added support for colors

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,3 +1,3 @@
 ./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model1.STEP --outputDir ./build/
-#./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model2.STEP
+./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model2.STEP --outputDir ./build/
 #./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputDir ./build/
