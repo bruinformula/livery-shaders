fe6df0fe9d470d373c106066092df7c9b4ffcf8f
Owen O'Malley <theowen@email.com>
2026-02-19 23:39:30 -0800

    Revise label hash table, add support for instancing

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,15 +1,3 @@
 ./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model1.STEP --outputDir ./build/
 #./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model2.STEP
-#!/bin/bash
-
-start=$(date +%s)
-
 #./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputDir ./build/
-
-end=$(date +%s)
-elapsed=$((end - start))
-
-echo "Time taken: ${elapsed} seconds"
-
-# For MK10 the cached usd write takes 68 seconds
-# this is a 700MB heavy Step file 
