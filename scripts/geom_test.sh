5189024c92a9dc844dd4f2e21ec52014854e5752
Owen O'Malley <theowen@email.com>
2026-02-22 23:11:07 -0800

    Added generation of wireframes

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,4 +1,4 @@
 ./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model1.STEP --outputDir ./build/
-#./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model2.STEP --outputDir ./build/
-#./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputDir ./build/
+./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model2.STEP --outputDir ./build/
+./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputDir ./build/
 #./build/bin/STEPConvertUSD --inputSTEPFile ../livery/car/step_files/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputDir ./build/
