0287d861ecc043c114536a89199147a20522a6ab
Owen O'Malley <theowen@email.com>
2026-02-26 18:31:18 -0800

    Revise handling of boundary nodes. All points are inserted in the face loop and not the edge loop to get around some loner points

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,6 +1,6 @@
-./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model1.STEP --outputDir ./build/
-#./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model2.STEP --outputDir ./build/
-#./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model3.STEP --outputDir ./build/
+./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model1.STEP --outputFile ./build/model1.usda
+#./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model2.STEP --outputFile ./build/model2.usda
+#./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model3.STEP --outputFile ./build/model3.usda
 
-./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputDir ./build/
-#./build/bin/STEPConvertUSD --inputSTEPFile ../livery/car/step_files/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputDir ./build/
+#./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputFile ./build/Mk10.usdc
+./build/bin/STEPConvertUSD --inputSTEPFile ../livery/car/step_files/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile ./build/Mk11.usdc
