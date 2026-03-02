0859db43ca05dd8a89dbbcf2c3d22e3c32623743
Owen O'Malley <theowen@email.com>
2026-03-02 15:48:30 -0800

    Added generation of sketch curves

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,6 +1,7 @@
-./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model1.STEP --outputFile ./build/model1.usda
+#./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model1.STEP --outputFile ./build/model1.usda
 #./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model2.STEP --outputFile ./build/model2.usda
-#./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model3.STEP --outputFile ./build/model3.usda
+./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model3.STEP --outputFile ./build/model3.usdc
+./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model4.STEP --outputFile ./build/model4.usdc
 
-./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputFile ./build/Mk10.usdc
+#./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputFile ./build/Mk10.usdc
 #./build/bin/STEPConvertUSD --inputSTEPFile ../livery/car/step_files/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile ./build/Mk11.usdc
