bb5b31e69739e5767af1a7aba785c63dd90479d0
Owen O'Malley <theowen@email.com>
2026-03-05 22:27:41 -0800

    Added variant export

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,7 +1,10 @@
 ./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model1.STEP --outputFile ./build/model1.usdc
+./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model1.STEP --outputFile ./build/model1-regular.usdc
+#./build/bin/STEPConvertUSDVariant --inputSTEPFile ./test/step/model1.STEP --outputFile ./build/model1-variant.usda --config scripts/low.config --config scripts/high.config 
+
 ./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model2.STEP --outputFile ./build/model2.usdc
 ./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model3.STEP --outputFile ./build/model3.usdc
 ./build/bin/STEPConvertUSD --inputSTEPFile ./test/step/model4.STEP --outputFile ./build/model4.usdc
 
-./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputFile ./build/Mk10.usdc
+#./build/bin/STEPConvertUSD --inputSTEPFile ../../Mk10/MK10.STEP --outputFile ./build/Mk10.usdc
 #./build/bin/STEPConvertUSD --inputSTEPFile ../livery/car/step_files/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile ./build/Mk11.usdc
