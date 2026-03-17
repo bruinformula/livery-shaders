d07af414ddd9fc192f4ae3dda151a98a1536c05d
Owen O'Malley <theowen@email.com>
2026-03-17 02:03:27 -0700

    Write prim data in SdfChangeBlock. rewrite cmake build for schema

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -7,4 +7,4 @@
 #./build/bin/StepConvertUsd --inputStepFile ./test/step/model4.Step --outputFile ./build/model4.usdc
 
 #./build/bin/StepConvertUsd --inputStepFile $MK10_ROOT/MK10.Step --outputFile ./build/Mk10.usdc
-#./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/car/step_files/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile ./build/Mk11.usdc
+./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/car/step_files/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile ./build/Mk11.usdc
