ae4836a0a51b93598d413e13f5b1fac1b81c45bb
Owen O'Malley <theowen@email.com>
2026-03-23 10:28:21 -0400

    Add Step units. Small touchups to STEP export. revise render vis rules

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -4,10 +4,10 @@
 
 ./build/bin/StepConvertUsd --inputStepFile ./test/step/model5.STEP --outputFile ./build/model5.usdc
 
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model2.STEP --outputFile ./build/model2.usdc
+./build/bin/StepConvertUsd --inputStepFile ./test/step/model2.STEP --outputFile ./build/model2.usdc --renderPurposeThreshold 100.0
 #./build/bin/StepConvertUsd --inputStepFile ./test/step/model3.STEP --outputFile ./build/model3.usdc
 #./build/bin/StepConvertUsd --inputStepFile ./test/step/model4.STEP --outputFile ./build/model4.usdc
 
 #./build/bin/StepConvertUsd --inputStepFile $MK10_ROOT/MK10.STEP --outputFile ./build/Mk10.usdc
-./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/car/step_files/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile ./build/Mk11-1.usdc
-./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/car/step_files/MK11-TOP-002-A-TOP_ASSEMBLY.STP --outputFile ./build/Mk11-2.usdc
+./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-old.usdc --renderPurposeThreshold 1.0
+./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-002-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-v1.usdc
