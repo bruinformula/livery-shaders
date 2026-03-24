dac9741c971d8581e5f13360483162efa7419463
Owen O'Malley <theowen@email.com>
2026-03-24 08:46:02 -0400

    Revise curve creation to seperate sampling from type

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -2,12 +2,13 @@
 #./build/bin/StepConvertUsd --inputStepFile ./test/step/model1.STEP --outputFile ./build/model1-regular.usdc
 #./build/bin/StepConvertUsdVariant --inputStepFile ./test/step/model1.STEP --outputFile ./build/model1-variant.usda --config scripts/low.config --config scripts/high.config 
 
-./build/bin/StepConvertUsd --inputStepFile ./test/step/model5.STEP --outputFile ./build/model5.usdc
 
-./build/bin/StepConvertUsd --inputStepFile ./test/step/model2.STEP --outputFile ./build/model2.usdc --renderPurposeThreshold 100.0
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model3.STEP --outputFile ./build/model3.usdc
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model4.STEP --outputFile ./build/model4.usdc
+./build/bin/StepConvertUsd --inputStepFile ./test/step/model2.STEP --outputFile ./build/model2.usdc --sketchSampling underlying --sketchType catmullRom --wireframeSampling underlying --wireframeType catmullRom
+./build/bin/StepConvertUsd --inputStepFile ./test/step/model3.STEP --outputFile ./build/model3.usdc --sketchSampling underlying --sketchType catmullRom --wireframeSampling underlying --wireframeType catmullRom
+./build/bin/StepConvertUsd --inputStepFile ./test/step/model4.STEP --outputFile ./build/model4.usdc --sketchSampling underlying --sketchType catmullRom --wireframeSampling underlying --wireframeType catmullRom
+./build/bin/StepConvertUsd --inputStepFile ./test/step/model5.STEP --outputFile ./build/model5.usdc --sketchSampling underlying --sketchType catmullRom --wireframeSampling underlying --wireframeType catmullRom
+
 
 #./build/bin/StepConvertUsd --inputStepFile $MK10_ROOT/MK10.STEP --outputFile ./build/Mk10.usdc
-./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-old.usdc --renderPurposeThreshold 1.0
-./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-002-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-v1.usdc
+#./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-old.usdc --renderPurposeThreshold 1.0
+#./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-002-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-v1.usdc
