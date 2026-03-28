2e6aeee3731e8ee541c45c73959b0176acdc7624
Owen O'Malley <theowen@email.com>
2026-03-28 09:49:18 -0500

    Refactor Step export to use params embedded in usd schema

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,20 +1,6 @@
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model1.STEP --outputFile ./build/model1.usdc
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model1.STEP --outputFile ./build/model1-regular.usdc
-#./build/bin/StepConvertUsdVariant --inputStepFile ./test/step/model1.STEP --outputFile ./build/model1-variant.usda --config scripts/low.config --config scripts/high.config 
 
 
-./build/bin/StepConvertUsd --inputStepFile ./test/step/model2.STEP --outputFile ./build/model2.usdc --sketchSampling underlying --sketchType catmullRom --wireframeSampling underlying --wireframeType catmullRom
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model3.STEP --outputFile ./build/model3.usdc --sketchSampling underlying --sketchType catmullRom --wireframeSampling underlying --wireframeType catmullRom
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model4.STEP --outputFile ./build/model4.usdc
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model5.STEP --outputFile ./build/model5.usdc 
+./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model.usda
 
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model4.STEP --outputFile ./build/model4-1.usdc --sketchSampling underlying --sketchType linear --wireframeSampling underlying --wireframeType linear
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model4.STEP --outputFile ./build/model4-2.usdc --sketchSampling underlying --sketchType catmullRom --wireframeSampling underlying --wireframeType catmullRom
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model4.STEP --outputFile ./build/model4-3.usdc --sketchSampling resampled --sketchType linear --wireframeSampling resampled --wireframeType linear
-#./build/bin/StepConvertUsd --inputStepFile ./test/step/model4.STEP --outputFile ./build/model4-4.usdc --sketchSampling resampled --sketchType catmullRom --wireframeSampling resampled --wireframeType catmullRom
-
-
-#./build/bin/StepConvertUsd --inputStepFile $MK10_ROOT/MK10.STEP --outputFile ./build/Mk10.usdc
-./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile ./build/mk11.usdc --renderPurposeThreshold 1.0
-#./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-old.usdc --renderPurposeThreshold 1.0
-#./build/bin/StepConvertUsd --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-002-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-v1.usdc
+#./build/bin/StepConvertUsdPlain --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-old.usdc --renderPurposeThreshold 1.0
+#./build/bin/StepConvertUsdPlain --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-002-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-v1.usdc
