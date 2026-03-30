3eaf4f59ecda367d09b9ef24a978f72c51bd5c01
Owen O'Malley <theowen@email.com>
2026-03-30 17:49:07 -0600

    Add support for targeting specific prims

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,9 +1,9 @@
 
-
 ./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model.usda
-./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model_variant.usda
+./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model.usda --prototype rod0
+#./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model_variant.usda
 
-./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model_variant.usda --variantSet LOD --variant high
+#./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model_variant.usda --variantSet LOD --variant high
 
 
 #./build/bin/StepConvertUsd --inputUsdFile $MK11_ROOT/mk11/geometry/model/model-v1/model-v1.usda
