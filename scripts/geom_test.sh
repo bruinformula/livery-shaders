fa99bcb7f331fef97ecb80abf8282f49e97ded63
Owen O'Malley <theowen@email.com>
2026-03-29 21:56:14 -0500

    Add support for variant sets in export

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,6 +1,7 @@
 
 
 ./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model.usda
+./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model_variant.usda
 
 #./build/bin/StepConvertUsdPlain --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-old.usdc --renderPurposeThreshold 1.0
 #./build/bin/StepConvertUsdPlain --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-002-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-v1.usdc
