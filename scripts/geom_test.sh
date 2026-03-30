5f80c34b7bb2696cdc34df3a36deba560b3f70e5
Owen O'Malley <theowen@email.com>
2026-03-30 12:20:49 -0500

    Add support in command line to only work on one variant

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,7 +1,14 @@
 
 
-./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model.usda
-./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model_variant.usda
+#./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model.usda
+#./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model_variant.usda
+
+./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model_variant.usda --variantSet LOD --variant high
+
+
+#./build/bin/StepConvertUsd --inputUsdFile $MK11_ROOT/mk11/geometry/model/model-v1/model-v1.usda
+#./build/bin/StepConvertUsd --inputUsdFile $MK11_ROOT/mk11/geometry/model/model-old/model-old.usda
+
 
 #./build/bin/StepConvertUsdPlain --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-001-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-old.usdc --renderPurposeThreshold 1.0
 #./build/bin/StepConvertUsdPlain --inputStepFile $MK11_ROOT/mk11/geometry/model/step/MK11-TOP-002-A-TOP_ASSEMBLY.STP --outputFile $MK11_ROOT/mk11/geometry/model/model-v1.usdc
