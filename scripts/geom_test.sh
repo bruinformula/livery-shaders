c514f807110c6cb083e4799a7ab54af72f3d394e
Owen O'Malley <theowen@email.com>
2026-04-01 20:02:09 -0700

    Revise commandline args. Change clearing conditions for prim overwrites. Remove automatic "default" variant intersertion"

--- scripts/geom_test.sh
+++ scripts/geom_test.sh
@@ -1,10 +1,10 @@
 
-./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model.usda
-#./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model.usda --prototype rod0
-./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model_variant.usda
+StepConvertUsd -i ./test/step/wonderful_model.usda -v
+StepConvertUsd -i ./test/step/wonderful_model_variant.usda -p /Wonderful{LOD=high}
+StepConvertUsd -i ./test/step/wonderful_model_variant_variant.usda -v
+StepConvertUsd -i ./test/step/wonderful_model_variant_variant.usda -p /Wonderful{LOD=high}Prototypes/rod0{quality=final} -v
+StepConvertUsd -i ./test/step/wonderful_model_variant_variant.usda -p /Wonderful{LOD=low}Prototypes/rod0{quality=final} -v
+StepConvertUsd -i ./test/step/wonderful_model_variant_variant.usda -p /Wonderful{LOD=high} -p /Wonderful{LOD=high}Prototypes/rod0{quality=low} -v
 
-#./build/bin/StepConvertUsd --inputUsdFile ./test/step/wonderful_model_variant.usda --variantSet LOD --variant high
-
-
-#./build/bin/StepConvertUsd --inputUsdFile $MK11_ROOT/mk11/geometry/model/model-v1/model-v1.usda
-#./build/bin/StepConvertUsd --inputUsdFile $MK11_ROOT/mk11/geometry/model/model-old/model-old.usda
+#StepConvertUsd --inputUsdFile $MK11_ROOT/mk11/geometry/model/model-v1/model-v1.usda
+#StepConvertUsd --inputUsdFile $MK11_ROOT/mk11/geometry/model/model-old/model-old.usda
